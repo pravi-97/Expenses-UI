0cf21eac16c7fff24b339f806a739ce74f0ee8c4
pravi-97 <praveesh.894@gmail.com>
2024-05-02 20:58:14 +0530

    add new fields

--- src/Error.jsx
+++ src/Error.jsx
@@ -1,5 +1,13 @@
+import './styles/Error.css'
 const Error = () => {
-    return ( <div>Error</div> );
-}
- 
+  return (
+    <>
+      <div className="error-container">
+        <h1>404</h1>
+        <p>Page Not Found!</p>
+      </div>
+    </>
+  );
+};
+
 export default Error;
--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,21 +1,7 @@
 import AuthLogin from './components/authLogin';
-import { useEffect } from "react";
-import axios from "axios";
 import "./styles/Welcome.css";
 
 const WelcomePage = () => {
-    const API_URL = "https://expensetracker-lhsl.onrender.com/";
-    useEffect(() => {
-      axios
-        .get(`${API_URL}ping`)
-        .then((response) => {
-          console.log(response.statusText);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    }, []);
-
     return (
       <div className="container">
         <main className="welcome-content">
