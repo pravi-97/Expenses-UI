ce2d34350bc32342c6a4088df9ab276e5cd4e9b5
pravi-97 <129660831+pravi-97@users.noreply.github.com>
2024-10-02 16:57:43 +0530

    Revert "update with new chart"

--- src/pages/Month.jsx
+++ src/pages/Month.jsx
@@ -3,11 +3,8 @@ import axios from "axios";
 import { AgChartsReact } from "ag-charts-react";
 import { useAuth0 } from "@auth0/auth0-react";
 import Loader from "./Loader";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
 
 const Month = () => {
-  const [data, setData] = useState([]) 
   const { user } = useAuth0();
   const API_URL = import.meta.env.VITE_API_URL;
   const [isLoading, setIsLoading] = useState(false);
@@ -18,8 +15,7 @@ const Month = () => {
         const response = await axios.get(
           `${API_URL}month?userid=${user.sub.replace("auth0|", "")}`
         );
-        // setOptions({ ...options, data: response.data });
-        setData(response.data);
+        setOptions({ ...options, data: response.data });
       } catch (error) {
         console.error("Error fetching data:", error);
       } finally {
@@ -29,41 +25,26 @@ const Month = () => {
     fetchData();
   }, []);
 
-  const CustomTooltip = ({ active, payload }) => {
-    if (active && payload && payload.length) {   
-      return (
-        <div className="custom-tooltip">
-          <p className="line_1" style={{backgroundColor:payload[0].color}}>{`Month and Year : ${payload[0].payload.formatted_date}`}</p>
-          <p className="line_2">{`Total Expense : $${payload[0].value}`}</p>
-        </div>
-      );
-    }
-    return null;
-  };
-
+  const [options, setOptions] = useState({
+    title: {
+      text: "Monthly Expenses",
+    },
+    series: [
+      {
+        type: "line",
+        xKey: "formatted_date",
+        yKey: "price",
+        yName: "Month and Year",
+      },
+    ],
+    background: {
+      fill: "transparent",
+    },
+  });
   return (
     <div>
       {isLoading ? <div><Loader /></div> : (<div className="container" style={{height:"100vh"}}>
-        <ResponsiveContainer width="100%" height="90%">
-        <LineChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="formatted_date" />
-          <YAxis dataKey="price" />
-          <Tooltip content={<CustomTooltip />}/>
-          <Legend />
-          <Line type="monotone" dataKey="price" stroke="#8884d8" activeDot={{ r: 8 }} />
-        </LineChart>
-      </ResponsiveContainer>
+        <AgChartsReact options={options}/>
       </div>)}
     </div>
   );
